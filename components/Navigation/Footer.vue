9bb092680405440655c62c057eeeb3536c292a2d
mubashar46 <mubashar@softsquare.io>
2021-02-18 12:46:50 +0500

    add scss folder structure

---
+++ components/Navigation/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <v-btn
+    class="btn-class"
+  >
+    Primary Button
+  </v-btn>
+  <span class="my-class" >
+      Footer
+  </span>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Footer.vue"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/sass/main.scss";
+.my-class {
+  width: 90%;
+  height: 2px;
+  color: $green; // Here we are using the variable
+border-radius: 5px;
+}
+.btn-class{
+  color: $pink;
+  background-color: $green;
+}
+</style>
---
+++ components/Navigation/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+<!--  {{ $t('home') }}-->
+  <v-app>
+    <v-toolbar >
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items >
+        <v-btn
+          color="success"
+          v-for="item in menuItems"
+          :key="item.title"
+         >
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Navbar.vue",
+  data(){
+    return {
+      appTitle: 'Wagenabled App',
+      sidebar: false,
+      menuItems: [
+        // { title: 'Home', path: '/home', icon: 'home' },
+        { title: this.$i18n.t('about_us')},
+        { title: this.$i18n.t('find_a_pet_pro')},
+        { title: this.$i18n.t('watch_and_learn')},
+        { title: this.$i18n.t('login')},
+        { title: this.$i18n.t('sign_up')},
+
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
