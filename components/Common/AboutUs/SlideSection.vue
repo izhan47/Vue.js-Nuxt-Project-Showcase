2587290634851f71fc59f82b19f0c106b3ab00e7
mubashar46 <mubashar@softsquare.io>
2021-03-09 18:44:22 +0500

    add slider

--- components/Common/AboutUs/SlideSection.vue
+++ components/Common/AboutUs/SlideSection.vue
@@ -7,30 +7,15 @@
           <v-row justify="center">
             <v-col col="12" md="12" sm="12">
               <v-card
-                class="mx-auto my-12 card-radius"
-              >
-                <div class="center-align pa-8">
-                  <img class="img-fluid card-img-height" src="/images/About/Aly-with-dogs.png"/>
-                </div>
+                class="mx-auto my-12 card-radius">
 
-                <!--              <div v-swiper="swiperOption" class="w-5/6 ml-auto relative" :loadtheme="false">-->
-                <!--                <div class="swiper-wrapper">-->
-                <!--                  <div class="swiper-slide" :key="banner" v-for="banner in banners">-->
-                <!--                    &lt;!&ndash; Render original HTML in server, render Swiper in browser (client) &ndash;&gt;-->
-                <!--                    <v-img class="h-64 w-64" :src="banner" ></v-img>-->
-                <!--                  </div>-->
-                <!--                </div>-->
-                <!--                <div class="swiper-button-prev" slot="button-prev"></div>-->
-                <!--                <div class="swiper-button-next" slot="button-next"></div>-->
-                <!--              </div>-->
+                  <VueSlickCarousel class="custom-padding " v-bind="settings">
+                    <div class="" v-for="item in banners">
+                      <img class="image-slider img-fluid " :src="item">
+                    </div>
+                  </VueSlickCarousel>
 
-                <!--              <swiper class="swiper" :options="Option">-->
-                <!--                <swiper-slide>Slide 1</swiper-slide>-->
-                <!--                <swiper-slide>Slide 2</swiper-slide>-->
-                <!--                <swiper-slide>Slide 3</swiper-slide>-->
-                <!--                <swiper-slide>Slide 4</swiper-slide>-->
-                <!--                <div class="swiper-pagination" slot="pagination"></div>-->
-                <!--              </swiper>-->
+
 
                 <v-card-title class="card-heading display mb-5">{{ $t('slide_heading') }}</v-card-title>
                 <v-card-text  class="center-align ">
@@ -42,10 +27,6 @@
                 <v-card-text class="center-align">
                   <p class="card-description mb-5">{{ $t('slide_description_3') }}</p>
                 </v-card-text>
-
-
-
-                <!--              <v-divider class="mx-4"></v-divider>-->
               </v-card>
             </v-col>
           </v-row>
@@ -58,60 +39,27 @@
 </template>
 
 <script>
-import { directive } from "vue-awesome-swiper";
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-// import 'swiper/dist/css/swiper.min.css'
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
   name: "slideSection.vue",
-  directives: {
-    swiper: directive,
-  },
   components:{
-    Swiper,
-    SwiperSlide
+    VueSlickCarousel
   },
   data() {
     return {
-      banners: ["/images/Column-Bone.png", "/images/About/Dogs-in-nature.png", "/images/About/Two-Dogs.png"],
-      swiperOption: {
-        slidesPerView: 3,
-        spaceBetween: -10,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 10
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 10
-          },
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 10
-          },
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 10
-          }
-        }
+      banners: ["/images/About/Aly-with-dogs.png", "/images/About/slider-2.png", "/images/About/slider-3.png",
+        "/images/About/slider-4.png", "/images/About/slider-5.png"],
+      settings: {
+        arrows: false,
+        dots: true,
+        autoplay:true,
+        vertical:false,
+        speed:500,
+        horizontal:true
       },
-     Option: {
-        pagination: {
-          el: '.swiper-pagination',
-          dynamicBullets: true
-        }
-      }
     };
   },
 }
@@ -119,13 +67,17 @@ export default {
 
 <style lang="scss" scoped>
 @import "~/assets/sass/main.scss";
-
 .wave-img{
   background-image: url("/images/About/shape.png");
   background-position: top;
   background-size: cover;
   background-repeat: no-repeat;
   padding-top: 5rem;
+}
+.custom-padding{
+  padding: 40px 30px 0 30px;
+ margin: auto;
+  max-width: 800px;
 }
 .slide-section{
   max-width: 1140px;
@@ -142,9 +94,8 @@ export default {
   text-shadow: 0 10px 20px rgba(0, 0, 0, 0.07);
 }
 .image-slider{
-  width: 650px;
-  height: 350px;
-  color:  #332E80;
+  height: auto;
+  color:  $purple;
 }
 .display{
   display: block;
@@ -179,11 +130,5 @@ export default {
     position: relative;
   }
 }
-.card-img-height{
-  max-height:400px;
-}
 
-.swiper-pagination-bullet.swiper-pagination-bullet-active::after {
-  @apply w-48 h-48 ;
-}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/i18n.js' },
-    { src: '~/plugins/vue-awesome-swiper.js' },
     { src: "~/plugins/aos.js", mode: "client" },
   ],
 
