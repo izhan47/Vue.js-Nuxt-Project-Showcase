0e9f7623fcf95009ffbeb387513f35c2a88ac597
M Izhan <izhan@softsquare.io>
2021-05-11 16:24:45 +0500

    doc-changes done

--- components/Common/Home/petFunctions.vue
+++ components/Common/Home/petFunctions.vue
@@ -1,84 +1,118 @@
 <template>
-<div class="grey-section">
+  <div class="grey-section">
     <div class="custom-container custom-height custom-padding" id="animate">
       <div class="function-heading mb-5">
         <h2 class="heading text-center">
-          {{ $t('for') }} {{ $t('pet_people') }}{{ $t('coma') }} {{ $t('by') }} {{ $t('pet_people') }}
+          {{ $t("only_best_for_pets") }}
         </h2>
       </div>
-      <div  class="description text-center space">
-        <p>{{ $t('pet_functionality_description') }}</p>
+      <div class="description text-center space">
+        <p>{{ $t("pet_functionality_description") }}</p>
       </div>
       <v-row justify="center">
         <v-col cols="12" md="4" sm="12">
           <v-card class="green-section card-radius pa-6">
             <div>
-              <h2 class="card-heading">{{ $t('find_a') }}</h2>
-              <h2 class="card-heading ">{{ $t('pet_professional') }}</h2>
+              <h2 class="card-heading">{{ $t("find_a") }}</h2>
+              <h2 class="card-heading ">{{ $t("local_business") }}</h2>
             </div>
             <div class="card-arrow-icon green-section">
-              <v-btn  large icon color="white" class="circle" @click="$router.push('/pet-category')"><v-icon>mdi-chevron-right</v-icon></v-btn>
+              <v-btn
+                large
+                icon
+                color="white"
+                class="circle"
+                @click="$router.push('/pet-category')"
+                ><v-icon>mdi-chevron-right</v-icon></v-btn
+              >
             </div>
             <div class="card-img ">
-              <img class="img-fluid img-height"
-                   src="/images/Column-Bone.png" alt="logo">
+              <img
+                class="img-fluid img-height"
+                src="/images/Column-Bone.png"
+                alt="logo"
+              />
             </div>
           </v-card>
         </v-col>
         <v-col cols="12" md="4" sm="12">
           <v-card class="purple-section card-radius pa-6">
             <div>
-              <h2 class="card-heading">  {{ $t('discover_new') }}</h2>
-              <h2 class="card-heading ">{{ $t('pet_products') }}</h2>
+              <h2 class="card-heading">{{ $t("discover_best") }}</h2>
+              <h2 class="card-heading ">{{ $t("pet_products") }}</h2>
             </div>
             <div class="card-arrow-icon purple-section">
-              <v-btn  large icon color="white" class="circle" @click="$router.push('/product-reviews')"><v-icon  >mdi-chevron-right</v-icon></v-btn>
+              <v-btn
+                large
+                icon
+                color="white"
+                class="circle"
+                @click="$router.push('/product-reviews')"
+                ><v-icon>mdi-chevron-right</v-icon></v-btn
+              >
             </div>
             <div class="card-img">
-              <img  class="img-fluid img-height" src="/images/Column-Food-Dish.png" alt="logo">
+              <img
+                class="img-fluid img-height"
+                src="/images/Column-Food-Dish.png"
+                alt="logo"
+              />
             </div>
           </v-card>
         </v-col>
         <v-col cols="12" md="4" sm="12">
           <v-card class="pink-section card-radius pa-6">
             <div>
-              <h2 class="card-heading">  {{ $t('explore_pet_care') }}</h2>
-              <h2 class="card-heading ">{{ $t('advice') }}</h2>
+              <h2 class="card-heading">{{ $t("explore_pet_care") }}</h2>
+              <h2 class="card-heading ">{{ $t("advice") }}</h2>
             </div>
             <div class="card-arrow-icon pink-section">
-              <v-btn  large icon color="white" class="circle" @click="$router.push('/watch-learn')"><v-icon>mdi-chevron-right</v-icon></v-btn>
+              <v-btn
+                large
+                icon
+                color="white"
+                class="circle"
+                @click="$router.push('/watch-learn')"
+                ><v-icon>mdi-chevron-right</v-icon></v-btn
+              >
             </div>
             <div class="">
-              <img class="img-fluid comment-img" src="/images/Column-Speech-Bubbles.png" alt="logo">
+              <img
+                class="img-fluid comment-img"
+                src="/images/Column-Speech-Bubbles.png"
+                alt="logo"
+              />
             </div>
           </v-card>
         </v-col>
       </v-row>
       <div class="search-img" id="dog-movement">
-        <img class="img-fluid" src="/images/Wag-Enabled-HP-Dog.png" alt="logo">
+        <img
+          class="img-fluid"
+          src="/images/Wag-Enabled-HP-Dog.png"
+          alt="logo"
+        />
       </div>
     </div>
-
-</div>
-
+  </div>
 </template>
 
 <script>
 export default {
   name: "petFunctions.vue",
-  mounted(){
-    if(process.browser){
-      let dog = document.getElementById('dog-movement');
-      ;(function(){
-        let throttle = function(type, name, obj){
+  mounted() {
+    if (process.browser) {
+      let dog = document.getElementById("dog-movement");
+      (function() {
+        let throttle = function(type, name, obj) {
           let object = obj || window;
           let running = false;
-          let func = function(){
-            if (running){
+          let func = function() {
+            if (running) {
               return;
             }
             running = true;
-            requestAnimationFrame(function(){
+            requestAnimationFrame(function() {
               object.dispatchEvent(new CustomEvent(name));
               running = false;
             });
@@ -87,68 +121,68 @@ export default {
         };
         throttle("scroll", "optimizedScroll");
       })();
-      window.addEventListener("optimizedScroll", function(){
-        let offset = window.pageYOffset-document.getElementById('animate').offsetTop
-        if(offset<350){
-          dog.style.transform = "translate(-" + (offset)  + "px,0px)";
+      window.addEventListener("optimizedScroll", function() {
+        let offset =
+          window.pageYOffset - document.getElementById("animate").offsetTop;
+        if (offset < 350) {
+          dog.style.transform = "translate(-" + offset + "px,0px)";
         }
-        if(window.screen.width < 769){
+        if (window.screen.width < 769) {
           dog.style.transform = "none";
         }
-      })
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "~/assets/sass/main.scss";
 
-.custom-padding{
-  padding: 8rem 12px 12px 12px
+.custom-padding {
+  padding: 8rem 12px 12px 12px;
 }
-.function-heading{
-  h2{
+.function-heading {
+  h2 {
     font-size: $font-size-45;
   }
 }
-.card-heading{
+.card-heading {
   color: $white;
-  font-family:  $font-family-primary;
+  font-family: $font-family-primary;
   font-size: $font-size-25;
   font-weight: $font-weight-400;
   text-align: left;
 }
 
-.card-arrow-icon{
+.card-arrow-icon {
   size: 19px;
   color: $white;
   text-align: left;
 }
-.circle::v-deep.v-btn{
-  background-color:rgb(256,256,256,0.7);
+.circle::v-deep.v-btn {
+  background-color: rgb(256, 256, 256, 0.7);
   border-radius: 50%;
   height: 38px;
   width: 38px;
-  @media (max-width:768px) {
+  @media (max-width: 768px) {
     height: 30px;
     width: 30px;
   }
 }
-.card-img{
+.card-img {
   margin-top: 7rem;
 }
-.comment-img{
+.comment-img {
   margin-top: 1rem;
   max-height: $img-max-height-220;
 }
-.search-img{
-  margin-top:2rem;
+.search-img {
+  margin-top: 2rem;
 
   text-align: right;
-  img{
+  img {
     max-height: $img-max-height-255;
   }
 }
 </style>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-<div>
-  <pet-functions></pet-functions>
-  <about></about>
-  <location-search @filter-data="filterData"></location-search>
-  <care-advice></care-advice>
-  <featured-product></featured-product>
-  <banner-search></banner-search>
-  <news-letter></news-letter>
-</div >
+  <div>
+    <pet-functions></pet-functions>
+    <about></about>
+    <location-search @filter-data="filterData"></location-search>
+    <care-advice></care-advice>
+    <featured-product></featured-product>
+    <banner-search></banner-search>
+    <news-letter></news-letter>
+  </div>
 </template>
 <script>
 import BannerSearch from "@/components/Common/Home/bannerSearch";
@@ -18,31 +18,35 @@ import CareAdvice from "@/components/Common/Home/careAdvice";
 import FeaturedProduct from "@/components/Common/Home/featuredProduct";
 import NewsLetter from "@/components/Common/Home/newsLetter";
 export default {
-  name:"index",
-  components:{
-    BannerSearch,PetFunctions,About,LocationSearch,CareAdvice,FeaturedProduct,NewsLetter
+  name: "index",
+  components: {
+    BannerSearch,
+    PetFunctions,
+    About,
+    LocationSearch,
+    CareAdvice,
+    FeaturedProduct,
+    NewsLetter
   },
-  data(){
-    return{
-      page:1
-    }
+  data() {
+    return {
+      page: 1
+    };
   },
   created() {
-    let filters= {
-      data:{},
-      page:this.page,
-    }
-    this.$store.dispatch('petProList',filters)
-    this.$store.dispatch('petCategories')
+    let filters = {
+      data: {},
+      page: this.page
+    };
+    this.$store.dispatch("petProList", filters);
+    this.$store.dispatch("petCategories");
   },
   methods: {
     filterData(filters) {
-      this.$store.dispatch('petProList',filters)
-    },
+      this.$store.dispatch("petProList", filters);
+    }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
