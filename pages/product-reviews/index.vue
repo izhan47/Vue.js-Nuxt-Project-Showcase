6a0630901ccbf731fd6691c778d9b81822f4420d
M Izhan <izhan@softsquare.io>
2021-05-31 19:28:22 +0500

    business nature added

--- pages/product-reviews/index.vue
+++ pages/product-reviews/index.vue
@@ -3,15 +3,17 @@
     <div class="bg-review-img">
       <div class="custom-container center-center custom-padding ">
         <div class="text-center">
-          <h2 class="heading  line-height mb-8">{{ $t('best_pet_products') }}</h2>
+          <h2 class="heading  line-height mb-8">
+            {{ $t("best_pet_products") }}
+          </h2>
         </div>
         <div class="space banner-description">
-          <p>{{$t('best_pet_products_description')}} </p>
+          <p>{{ $t("best_pet_products_description") }}</p>
         </div>
         <!--   Filter Section Start     -->
         <v-form class="search-form-filter" @submit.prevent="filterData">
           <div class="search-form-field">
-            <label>{{ $t('sort_by') }}</label>
+            <label>{{ $t("sort_by") }}</label>
             <v-select
               class="search-field mt-2"
               :items="sorting"
@@ -22,7 +24,7 @@
             ></v-select>
           </div>
           <div class="search-form-field">
-            <label >{{ $t('category') }}</label>
+            <label>{{ $t("category") }}</label>
 
             <v-select
               :placeholder="$t('all')"
@@ -39,20 +41,40 @@
                 <v-chip v-if="index === 0">
                   <span>{{ item.text }}</span>
                 </v-chip>
-                <span
-                  v-if="index === 1"
-                  class="grey--text caption"
-                >
-                    (+{{ categories.length - 1 }} others)
-                  </span>
+                <span v-if="index === 1" class="grey--text caption">
+                  (+{{ categories.length - 1 }} others)
+                </span>
               </template>
             </v-select>
-
-
-
           </div>
+
           <div class="search-form-field">
-            <label >{{ $t('keyword') }}</label>
+            <label>Nature of Business</label>
+
+            <v-select
+              :placeholder="$t('all')"
+              class="search-field-category mt-2"
+              :items="businessList"
+              v-model="business"
+              outlined
+              rounded
+              multiple
+              small-chips
+              @change="filterData()"
+            >
+              <template v-slot:selection="{ item, index }">
+                <v-chip v-if="index === 0">
+                  <span>{{ item.text }}</span>
+                </v-chip>
+                <span v-if="index === 1" class="grey--text caption">
+                  (+{{ businessList.length - 1 }} others)
+                </span>
+              </template>
+            </v-select>
+          </div>
+
+          <div class="search-form-field">
+            <label>{{ $t("keyword") }}</label>
             <v-text-field
               class="search-field cross-icon mt-2"
               :placeholder="$t('all')"
@@ -64,14 +86,14 @@
               clearable
             ></v-text-field>
           </div>
-          <div >
+          <div>
             <v-btn
               class="purple-section  search-btn"
               outlined
               large
               @click="filterData"
             >
-              {{ $t('search') }}
+              {{ $t("search") }}
             </v-btn>
           </div>
         </v-form>
@@ -82,7 +104,14 @@
 
     <div class="custom-container  space" v-if="reviewData.length">
       <v-row>
-        <v-col cols="12" md="4" sm="12" v-for="(data,i) in reviewData" :key="i" class="mt-8">
+        <v-col
+          cols="12"
+          md="4"
+          sm="12"
+          v-for="(data, i) in reviewData"
+          :key="i"
+          class="mt-8"
+        >
           <product-review-card :item="data"></product-review-card>
         </v-col>
       </v-row>
@@ -99,12 +128,15 @@
       </div>
     </div>
     <div v-else class="text-center">
-      <img class="img-height img-fluid"  src="/images/Auth/Column-3-Dog.png" alt="logo" />
-      <h2 class="heading">{{$t('nothing_here')}}</h2>
+      <img
+        class="img-height img-fluid"
+        src="/images/Auth/Column-3-Dog.png"
+        alt="logo"
+      />
+      <h2 class="heading">{{ $t("nothing_here") }}</h2>
     </div>
 
     <!--  card-section-end   -->
-
   </div>
 </template>
 
@@ -113,80 +145,117 @@ import ProductReviewCard from "@/components/ProductReviewCard";
 
 export default {
   name: "index.vue",
-  data(){
-    return{
+  data() {
+    return {
       page: 1,
-      sorting: ['Latest', 'Popular','Deal offered'],
-      categories:[],
-      form:{
-        category_id:[],
-        sort_by:'Latest',
-        search:''
-      },
-    }
+      sorting: ["Latest", "Popular", "Deal offered"],
+      business_list: [],
+      categories: [],
+      business: [],
+      form: {
+        category_id: [],
+        sort_by: "Latest",
+        search: "",
+        business_id: []
+      }
+    };
   },
-  components:{ProductReviewCard},
-  computed:{
-    reviewData(){
-      return this.$store.state.review_list
+  components: { ProductReviewCard },
+  computed: {
+    reviewData() {
+      return this.$store.state.review_list;
     },
-    totalPage(){
-      return this.$store.state.total_page
+    totalPage() {
+      return this.$store.state.total_page;
     },
 
-    categoryList(){
-      let categories= this.$store.state.product_review_category_list
-      let arr = []
-      categories.forEach(function (data) {
+    categoryList() {
+      let categories = this.$store.state.product_review_category_list;
+      let arr = [];
+      categories.forEach(function(data) {
         arr.push({
-          'value':data.value,
-          'text': data.label,
-        })
-      })
-      return arr
+          value: data.value,
+          text: data.label
+        });
+      });
+      return arr;
+    },
+    businessList() {
+      return this.business_list.map(b => {
+        return {
+          value: b.value,
+          text: b.label
+        };
+      });
     }
   },
   created() {
-    let filters= {
-      form:{},
-      page:this.page,
+    if (this.$route.query.page) {
+      this.page = Number(this.$route.query.page);
     }
-    this.$store.dispatch('reviewList',filters)
-    this.$store.dispatch('reviewCategories')
+
+    let filters = {
+      form: {},
+      page: this.page
+    };
+    this.fetchBusinessNature();
+    this.$store.dispatch("reviewList", filters);
+    this.$store.dispatch("reviewCategories");
   },
-  methods:{
-    filterData(){
-      let arr = []
+  methods: {
+    filterData() {
+      let arr = [];
       this.categories.forEach(id => {
-        let x =  this.categoryList.find(e => e.value === id)
+        let x = this.categoryList.find(e => e.value === id);
         arr.push({
-          value : x.value,
-          label : x.text
-        })
-      })
-      this.form.category_id= JSON.stringify(Object.assign( arr));// convert array to string
+          value: x.value,
+          label: x.text
+        });
+      });
 
-      let filters= {
-        form:this.form,
-        page:this.page,
+      this.form.category_id = JSON.stringify(Object.assign(arr)); // convert array to string
+
+      this.form.business_id = this.business.map(business => {
+        const exist = this.business_list.find(b => b.value == business);
+        exist.label = exist.label.toLowerCase();
+        return exist;
+      });
+
+      this.$router.push({
+        query: {
+          page: this.page
+        }
+      });
+      let filters = {
+        form: this.form,
+        page: this.page
+      };
+
+      this.$store.dispatch("reviewList", filters);
+    },
+    async fetchBusinessNature() {
+      try {
+        const resp = await this.$axios.post("pet-pro/get-business-nature-list");
+        const { business_nature_list } = resp.data.data;
+        this.business_list = business_nature_list;
+      } catch (error) {
+        console.log(error);
       }
-
-      this.$store.dispatch('reviewList',filters)
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "~/assets/sass/main.scss";
-.bg-review-img{
+.bg-review-img {
   background-image: url("/images/Reviews/Product-Reviews-Banner-2.png");
   background-position: 10% 68%;
   //background-position: 10% 56%;
   background-size: cover;
   background-repeat: no-repeat;
   @media (max-width: 1440px) {
-    background-position:10% 82%;
+    background-position: 10% 82%;
   }
   @media (max-width: 1024px) {
     background-position: 10% 175%;
@@ -196,58 +265,57 @@ export default {
   }
 }
 
-.banner-description{
+.banner-description {
   max-width: 550px;
 }
-.search-field-category::v-deep .v-input__slot{
+.search-field-category::v-deep .v-input__slot {
   background: $white;
   min-height: 48px;
   box-shadow: unset !important;
   max-width: 273px;
   font-weight: $font-weight-bold;
-  font-family:$font-family-primary;
-  .v-text-field__slot{
+  font-family: $font-family-primary;
+  .v-text-field__slot {
     font-weight: $font-weight-bold;
   }
-  fieldset{
+  fieldset {
     color: rgba(0, 0, 0, 0.7) !important;
   }
 }
 .search-field-category::v-deep .v-label {
-  top:14px;
+  top: 14px;
 }
-.search-field-category::v-deep .v-input__append-inner{
+.search-field-category::v-deep .v-input__append-inner {
   margin-top: 12px;
 }
 
-.search-field::v-deep .v-input__slot{
+.search-field::v-deep .v-input__slot {
   background: $white;
   min-height: 48px;
   box-shadow: unset !important;
   max-width: 170px;
   font-weight: $font-weight-bold;
-  font-family:$font-family-primary;
-  .v-text-field__slot{
+  font-family: $font-family-primary;
+  .v-text-field__slot {
     font-weight: $font-weight-bold;
   }
 }
 
-
 .search-field::v-deep .v-label {
-  top:14px;
+  top: 14px;
 }
-.search-field::v-deep .v-input__append-inner{
+.search-field::v-deep .v-input__append-inner {
   margin-top: 12px;
 }
 
-.search-btn::v-deep.v-btn{
+.search-btn::v-deep.v-btn {
   min-width: 140px;
   height: 52px;
 }
-.img-height{
-  max-height:$img-max-height-250;
+.img-height {
+  max-height: $img-max-height-250;
 }
-.cross-icon::v-deep .v-input__append-inner{
+.cross-icon::v-deep .v-input__append-inner {
   margin-top: 0;
 }
 </style>
