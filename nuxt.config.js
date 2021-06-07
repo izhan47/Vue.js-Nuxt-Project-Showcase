1d1a340b8825426c5989c99fa0e45b3234bdad5f
M Izhan <izhan@softsquare.io>
2021-06-07 19:53:03 +0500

    fix the bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,13 +16,13 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-    // script: [
-    //   {
-    //     src:
-    //       "https://maps.googleapis.com/maps/api/js?key=AIzaSyBaxMfWKuh_m7up5CvIL-LF_EHJ_eWkRWI&libraries=places"
-    //   }
-    // ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
+      {
+        src:
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyBaxMfWKuh_m7up5CvIL-LF_EHJ_eWkRWI&libraries=places"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
